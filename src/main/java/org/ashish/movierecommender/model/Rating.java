b9c04304f5c230e2f1b0a276614e0591aab83f10
Ashish Sharda <ashishjsharda@gmail.com>
2025-01-16 05:31:13 -0500

    Add Rating and User models

---
+++ src/main/java/org/ashish/movierecommender/model/Rating.java
@@ -0,0 +1,25 @@
+package org.ashish.movierecommender.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "ratings")
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    private Integer rating;
+    private String review;
+}
